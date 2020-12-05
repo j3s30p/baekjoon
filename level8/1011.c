48e0a37787ca341ba52afff56a6977eb4a5b1b2d
j3s30p <j3s30p@j3s30ps-MacBook-Pro.local>
2020-12-05 23:44:27 +0900

    20201205

---
+++ level8/1011.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+int		ft_sqrt(int nb)
+{
+	unsigned int	n;
+
+	n = 2;
+	if (nb < 1)
+		return (0);
+	if (nb == 1)
+		return (1);
+	if (nb == 3)
+		return (2);
+	while (n * n <= (unsigned int)nb)
+	{
+		if (n * n == (unsigned int)nb)
+			return (n);
+		n++;
+	}
+	return (0);
+}
+
+int		ft_sqrt2(int nb)
+{
+	unsigned int	n;
+
+	n = 2;
+	if (nb < 1)
+		return (0);
+	if (nb == 1)
+		return (1);
+	while (n * n < (unsigned int)nb)
+		n++;
+	return (n);
+}
+
+int		checkNum(int d)
+{
+	int		num;
+
+	if (ft_sqrt(d))
+		return((2 * ft_sqrt(d)) - 1);
+	else
+	{
+		num = ft_sqrt2(d) - 1;
+		if (d <= num * num + num)
+			return (2 * num);
+		else
+			return (2 * num + 1);
+	}
+	return (1);
+}
+
+int		main(void)
+{
+	int		i;
+	int		test_case;
+	int		str_location, dest_location;
+
+	scanf("%d", &test_case);
+	for (i = 0; i < test_case; i++)
+	{
+		scanf("%d %d", &str_location, &dest_location);
+		printf("%d\n", checkNum(dest_location - str_location));
+	}
+	return (0);
+}
