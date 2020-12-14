20917d4a4c8f79c4b9e63c0745d5a23ad9fbc009
j3s30p <j3s30p@j3s30ps-MacBook-Pro.local>
2020-12-15 01:21:41 +0900

    20201214

--- level10/2447.c
+++ level10/2447.c
@@ -1,17 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void	makeStar(int n, char **arr)
+void	makeStar(int n, char **arr, int a, int b, int c)
 {
 	int		i, k;
+	int		box;
 
-	for (i = 0; i < n; i++)
+	box = b - a;
+	// a, b, c를 계속 줄여가기 때문에 a, b , c != 0일때 한정.
+	if (a && b && c)
 	{
-		for (k = 0; k < n; k++)
+		// 크기에 맞는 공백을 모두 채울때까지 반복
+		if (c < n)
 		{
-			if (i % 3 == 1 && k % 3 == 1)
-				arr[i][k] = ' ';
+			//실제로 공백을 주는 부분
+			for (i = c; i < c + box; i++)
+			{	
+				for (k = a; k < b; k++)
+					arr[i][k] = ' ';
+			}
+			//한칸씩 우측으로 이동
+			if (b + (2 * box) < n)
+			{
+				a = b + 2 * box;
+				b = a + box;
+				makeStar(n, arr, a, b, c);
+			}
+			//한칸씩 아래로 이동
+			else
+			{
+				c += 3 * box;
+				makeStar(n, arr, box, 2 * box, c);
+			}
 		}
+		//	c >= n이 되면 더 작은 부분으로 넘어간다.
+		else
+			makeStar(n, arr, box / 3, 2 * (box / 3), box / 3);
 	}
 }
 
@@ -22,6 +46,7 @@ int		main(void)
 	char	**arr;
 
 	scanf("%d", &n);
+	//n * n 만큼 동적할당
 	arr = (char **)malloc(sizeof(char *) * n);
 	for (i = 0; i < n; i++)
 		arr[i] = (char *)malloc(sizeof(char) * n);
@@ -30,13 +55,16 @@ int		main(void)
 		for (k = 0; k < n; k++)
 			arr[i][k] = '*';
 	}
-	makeStar(n, arr);
+	//별찍기 배열 완성
+	makeStar(n, arr, n / 3, (n / 3) * 2, n / 3);
+	//배열 출력
 	for (i = 0; i < n; i++)
 	{
 		for (k = 0; k < n; k++)
 			printf("%c", arr[i][k]);
 		printf("\n");
 	}
+	//동적할당 풀어주기
 	for (i = 0; i < n; i++)
 		free(arr[i]);
 	free(arr);
